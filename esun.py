5814be02c39025a7c64838012f21da5901181a43
Laura Elizabeth Giménez <laueligmz2024@gmail.com>
2025-04-03 02:51:01 +0200

    Update esun.py

--- esun.py
+++ esun.py
@@ -6,3 +6,27 @@ class Animal:
         return f"{self.nombre} hace n sonido"
     def moverse(self):
         return f"{self.nombre} se está moviendo"
+class Perro(Animal):
+    def __init__(self,nombre, edad, raza):
+        super().__init__(nombre,edad)
+        self.raza=raza
+
+    def hacer_sonido(self):
+        return f"{self.nombre} está ladrando"
+    
+    def se_para_en_2_patas(self):
+        return f"{self.nombre} se para en 2 patas"
+
+class Gallina(self):
+    def __init__(self,nombre,edad,tipo):
+        super().__init__(nombre,edad)
+        self.tipo=tipo
+
+    def hacer_sonido(self):
+        return f"{self.nombre} está cacareando"
+    def picotear(self):
+        return f"{self.nombre} picotea"
+    
+Animaldepreuba=Animal("loro",5)
+perro1=Perro("Toby",2,"caniche")
+gallna1=Gallina("Manchita",1,"Brahma")
